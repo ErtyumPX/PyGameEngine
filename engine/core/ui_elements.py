8e34557d7b444948357fb8a4b029c97dc5bc548c
ErtyumPX <49292808+ErtyumPX@users.noreply.github.com>
2023-10-18 18:33:09 +0300

    InputBox cursor adjusted
    Find next word functions added and now InputBoxes support going to the next word buy pressing CTRL + side arrow keys. There is a slight issue with the DELETE button but it will fixed later.

--- engine/core/ui_elements.py
+++ engine/core/ui_elements.py
@@ -1,22 +1,32 @@
-# TODO: InputBox should be refactored 
-# TODO: For all elements that are rendering more than 1 element, the sub/child elements should be rendered
+# [] TODO: InputBox should be refactored 
+# [] TODO: For all elements that are rendering more than 1 element, the sub/child elements should be rendered
 # on the parent surface but not the root surface, and parent and child surfaces should be updated only when
 # a factor that effects the render output changes inside the child or parent element
-# TODO: More elements should be added -> CheckBox, ToggleButton, Slider, Image etc
-# TODO: All interactables - button, checkbox, toggle button etc - should be inherited from the InteractableElement class
-# TODO: InputBox with multiline support 
+# [] TODO: More elements should be added -> CheckBox, ToggleButton, Slider, Image, RadioButton, DropDown List, ListBox etc.
+# [] TODO: Sub-elements for increasing the usability of other elements: Slider, ScrollBar, ToolTip,
+# ProgressBar ProgressRing, MessageBox service etc.
+# [] TODO: Navigational Elements: Layout, Grid, Tab, Menu, MenuBar, ToolBar, StatusBar, TreeView, ListView etc.
+# [] TODO: All interactables - button, checkbox, toggle button etc - should be inherited from the InteractableElement class
+# [] TODO: InputBox with multiline support 
 	# Main issues are: Cursor position, adding new lines and going to the next line when the word is too long
-# TODO: Hotkeys could be added and bound to interactable elements
+# [] TODO: Hotkeys could be added and bound to interactable elements
 
 import pygame
 from pygame import Surface
 from pygame.rect import Rect
 from enum import Enum
 import re
+from time import time as get_time
 
 EMPTY_FUNCTION = lambda: 0
 DEFAULT_FONT_PATH = 'data/SpaceMono-Regular.ttf'
-VALID_INPUT_CHARACTERS = r'[^\.A-Za-z0-9 _]'
+VALID_INPUT_CHARACTERS = r"[^a-zA-Z0-9., &%+-=^$'\"()\[\]{}*?!@#_/\\|:;<>~`]"
+INPUTBOX_TEXT_SIDE_MARGIN = 5
+INPUTBOX_HOVERING_CURSOR = pygame.SYSTEM_CURSOR_IBEAM
+INPUTBOX_CURSOR_BLINK_INTERVAL = 0.6 # In seconds
+
+_DEPRECATED_VALID_INPUT_CHARACTERS = r'[^\.A-Za-z0-9 _]'
+_DEPRECATED_INPUTBOX_CURSOR_CHARACTER = "|"
 
 """
 class InterfaceElement:
@@ -223,8 +233,8 @@ class Button(InterfaceElement):
 		self.pressed_kwargs: dict = pressed_kwargs
 
 		self.click_function: callable = click_fuction
-		self.args:tuple = args
-		self.kwargs:dict = kwargs
+		self.args: tuple = args
+		self.kwargs: dict = kwargs
 
 		InterfaceElement.__init__(self, root_surface, status, x, y, 
 								width, height, alignment)
@@ -232,10 +242,12 @@ class Button(InterfaceElement):
 							font_size, foreground, Alignment.CENTER)
 
 	def hover_event(self):
+		print("Hovered!")
 		self.phase = InteractablePhase.HOVER
 		self.hover_event(*self.hover_args, **self.hover_kwargs)
 	
 	def pressed_event(self):
+		print("Pressed!")
 		self.phase = InteractablePhase.PRESSED
 		self.pressed_event(*self.pressed_args, **self.pressed_kwargs)
 
@@ -256,64 +268,31 @@ class Button(InterfaceElement):
 			self.effect_surface.fill(self.pressed_color)
 		self.root_surface.blit(self.effect_surface, (self.rect.x, self.rect.y))
 
-"""
-valid = r'[^\.A-Za-z0-9 _]'
-class InputBox(pygame.sprite.Sprite, InterfaceElement):
-	def __init__(self, surface, x:int = 0, y:int = 0, width:int = 50, height:int = 20, font_size = 50, color=(0, 0, 0), default_text='', image=BLANK_WHITE_IMAGE, status:int = 0):
-		pygame.sprite.Sprite.__init__(self)
-		InterfaceElement.__init__(self, status)
-		self.surface = surface
-		self.image = pygame.transform.scale(image, (width, height))
-		self.x, self.y, self.width, self.height = x, y, width, height
-		self.selected = False
 
-		self.text = ''
-		self.default_text = default_text
-		self.color = color
-		self.font_size = min(height-10, font_size)
-		self.font = pygame.font.Font(DEFAULT_FONT_PATH, self.font_size)
-		self.text_box = self.font.render(self.text, True, color)
-
-	def add(self, char):
-		if not re.search(valid, char) and self.image.get_width() - self.text_box.get_width() > 15 + self.font_size/2:
-			self.text += char
-
-	def remove(self):
-		self.text = self.text[:-1]
-
-	def clear(self):
-		self.text = ''
-
-	def render(self):
-		self.surface.blit(self.image, (self.x, self.y))
-		if self.text == '':
-			self.text_box = self.font.render(self.default_text, True, (140,140,140))
-		else:
-			self.text_box = self.font.render(self.text, True, self.color)
-		self.surface.blit(self.text_box, (self.x + 7.5, self.y + 4))
-"""
-SIDE_MARGIN = 5
 class InputBox(InterfaceElement):
-	def __init__(self, root_surface, x:int = 0, y:int = 0, width:int = 100, height:int = 25, 
-				font_size=11, background:tuple = (190, 190, 190), 
-				foreground:tuple = (0, 0, 0), border_color:tuple = (0, 0, 0), 
-				border_width:int = 4, status:InterfaceStatus = InterfaceStatus.VISIBLE,
-				alignment:Alignment = Alignment.LEFT_TOP, 
-				place_holder_text="Enter Text...", place_holder_color:tuple = (80, 80, 80),
-				hovering_cursor: pygame.Cursor = pygame.cursors.diamond):
-		self.surface = Surface((width, height))
+	def __init__(self, root_surface, x: int = 0, y: int = 0, width: int = 100, height: int = 25, 
+				font_size: int = 11, background: tuple = (190, 190, 190), 
+				foreground:tuple = (0, 0, 0), border_color: tuple = (0, 0, 0), 
+				border_width: int = 4, status: InterfaceStatus = InterfaceStatus.VISIBLE,
+				alignment: Alignment = Alignment.LEFT_TOP, 
+				place_holder_text: str = "Enter Text...", place_holder_color: tuple = (80, 80, 80)):
+		self.surface: Surface = Surface((width, height))
 		self.surface.fill(background)
 		pygame.draw.rect(self.surface, border_color, (0, 0, width, height), border_width)
 
-		self.hovering_cursor: pygame.Cursor = hovering_cursor
 		self._selected: bool = False
+		self._cursor: int = 0 # Character index of the current cursor position
+		self.cursor_position_x: int = 0
+		self.last_blink_time: int = 0
+		self.is_blinking : bool = False
 
 		InterfaceElement.__init__(self, root_surface, status, x, y, 
 								width, height, alignment)
-		self.place_holder:Text = Text(root_surface, self.rect.left + SIDE_MARGIN, self.rect.centery, place_holder_text, 
+		self.place_holder: Text = Text(root_surface, self.rect.left + INPUTBOX_TEXT_SIDE_MARGIN, self.rect.centery, place_holder_text, 
 									font_size, place_holder_color, Alignment.LEFT)
-		self.text:Text = Text(root_surface, self.rect.left + SIDE_MARGIN, self.rect.centery, str(), 
+		self.text: Text = Text(root_surface, self.rect.left + INPUTBOX_TEXT_SIDE_MARGIN, self.rect.centery, str(), 
 									font_size, foreground, Alignment.LEFT)
+		self.text_char_size: tuple = self.text.font.size("E")
 	@property
 	def selected(self):
 		return self._selected
@@ -321,31 +300,108 @@ class InputBox(InterfaceElement):
 	@selected.setter
 	def selected(self, new_selected):
 		self._selected = new_selected
+		if new_selected == True:
+			self.last_blink_time = get_time()
+	
+	@property
+	def cursor(self):
+		return self._cursor
+
+	@cursor.setter
+	def cursor(self, new_cursor):
+		if new_cursor < 0:
+			new_cursor = 0
+		elif new_cursor > len(self.text.text):
+			new_cursor = len(self.text.text)
+		if self._cursor == new_cursor: return
+
+		change = new_cursor - self._cursor
+		self._cursor = new_cursor
+		self.cursor_position_x += change * self.text_char_size[0]
+		self.last_blink_time = get_time()
+		self.is_blinking = True
+
 
 	def add(self, char):
-		if re.search(VALID_INPUT_CHARACTERS, char):
+		if re.search(VALID_INPUT_CHARACTERS, char) or len(char) != 1:
 			return
 		new_text = self.text.text + char
-		if(self.text.font.size(new_text)[0] > self.rect.width - SIDE_MARGIN*2): # 5 pixel margin from each side
+		if(self.text.font.size(new_text)[0] > self.rect.width - INPUTBOX_TEXT_SIDE_MARGIN*2): # 5 pixel margin from each side
 			return
-		self.text.change_text_to(new_text)
+		# add the new character to the cursor position
+		self.text.change_text_to(self.text.text[:self.cursor] + char + self.text.text[self.cursor:])
+		self.cursor += 1
 
-	def remove(self):
+	def remove(self, direction: int = 1):
+		# Direction specifies the character removing direction
+		# if DELETE key is pressed, direction is -1, if BACKSPACE key is pressed, direction is 1
 		# This will be used when cursor is added
-		# self.text.change_text_to(self.text.text[:index] + self.text.text[index + 1:])
-		self.text.change_text_to(self.text.text[:-1])
-	
+		# TO DEVS: It's important to note when DELETE key is pressed, the cursor does not move
+		if len(self.text.text) > 0:
+			if direction > 0:
+				self.text.change_text_to(self.text.text[:self.cursor - 1] + self.text.text[self.cursor:])
+				self.cursor -= 1 * direction
+			else:
+				self.text.change_text_to(self.text.text[:self.cursor] + self.text.text[self.cursor + 1:])
+
+	def find_beginning_of_word(self, direction: int = 1) -> int:
+		word_index = self.cursor - 1 * direction
+		is_any_char_found = False
+		while word_index > 0 and word_index < len(self.text.text):
+			current_character = self.text.text[word_index]
+			if current_character != " ": is_any_char_found = True
+			if current_character == " " and is_any_char_found: break
+			word_index -= 1 * direction
+		if direction < 0:
+			word_index += 1
+		return word_index
+
+
+	def find_word_index(self, direction: int = 1) -> int:
+		is_word_found = False
+		word_index = self.cursor - 1 * direction
+		while word_index > 0 and word_index < len(self.text.text):
+			current_character = self.text.text[word_index]
+			if current_character != " ": 
+				is_word_found = True
+			word_index -= 1 * direction
+			if word_index < 0 or word_index >= len(self.text.text):
+				word_index += 1 * direction
+				break
+			current_character = self.text.text[word_index]
+			if current_character == " " and is_word_found:
+				word_index += 1 * direction
+				break
+		if direction < 0:
+			word_index += 1
+		return word_index
+
+	def remove_word(self, direction: int = 1):
+		removing_index = self.find_beginning_of_word(direction)
+		if direction > 0:
+			self.text.change_text_to(self.text.text[:removing_index] + self.text.text[self.cursor:])
+			self.cursor = removing_index
+		else:
+			self.text.change_text_to(self.text.text[:self.cursor] + self.text.text[removing_index:])
+
 	def clear(self):
 		self.text.change_text_to(str())
+		self.cursor = 0
 
 	def render(self):
 		if self.status == InterfaceStatus.INVISIBLE: return
 		self.root_surface.blit(self.surface, (self.rect.x, self.rect.y))
-		if len(self.text.text) == 0 and not self.selected:
-			self.place_holder.render()
-		else:
+		if len(self.text.text) > 0:
 			self.text.render()
-
+		elif not self.selected:
+			self.place_holder.render()
+		if self.selected and self.is_blinking:
+			line_beginning_position = (self.text.rect.left + self.cursor_position_x, self.text.rect.bottom)
+			line_ending_position = (self.text.rect.left + self.cursor_position_x, self.text.rect.bottom - self.text_char_size[1])
+			pygame.draw.line(self.root_surface, self.text.color, line_beginning_position, line_ending_position, 1)
+		if get_time() - self.last_blink_time > INPUTBOX_CURSOR_BLINK_INTERVAL:
+			self.last_blink_time = get_time()
+			self.is_blinking = not self.is_blinking
 
 
 def ProcessElements(events, pressed_keys, mouse_pos, elements:list = [], inputs:list = [], texts:list = []):
@@ -377,9 +433,27 @@ def ProcessElements(events, pressed_keys, mouse_pos, elements:list = [], inputs:
 			for input_box in inputs:
 				if input_box.selected:
 					if event.key == pygame.K_BACKSPACE:
-						if pressed_keys[pygame.K_LSHIFT] or pressed_keys[pygame.K_RSHIFT] or pressed_keys[pygame.K_LCTRL] or pressed_keys[pygame.K_RCTRL]:
-							input_box.clear()
+						if pressed_keys[pygame.K_LCTRL] or pressed_keys[pygame.K_RCTRL]:
+							input_box.remove_word(direction = 1)
 						else: 
-							input_box.remove()
+							input_box.remove(direction = 1)
+					if event.key == pygame.K_DELETE:
+						if pressed_keys[pygame.K_LCTRL] or pressed_keys[pygame.K_RCTRL]:
+							input_box.remove_word(direction = -1)
+						else: 
+							input_box.remove(direction = -1)
+					elif event.key == pygame.K_RETURN:
+						# TODO: will add ENTER_KEY event in case InputBox is wanted to be attached with an action
+						pass
+					elif event.key == pygame.K_LEFT:
+						if pressed_keys[pygame.K_LCTRL] or pressed_keys[pygame.K_RCTRL]:
+							input_box.cursor = input_box.find_beginning_of_word(direction = 1)
+						else:
+							input_box.cursor -= 1
+					elif event.key == pygame.K_RIGHT:
+						if pressed_keys[pygame.K_LCTRL] or pressed_keys[pygame.K_RCTRL]:
+							input_box.cursor = input_box.find_beginning_of_word(direction = -1)
+						else:
+							input_box.cursor += 1
 					else: 
 						input_box.add(event.unicode)
